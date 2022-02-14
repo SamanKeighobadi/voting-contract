ea357ffa8f54407fba4d586b988bbc98be874e82
saman keighobadi <saman.keighobadi@gmail.com>
2022-02-14 23:56:33 +0330

    definition functions

--- VotingContract.sol
+++ VotingContract.sol
@@ -1,13 +1,25 @@
-pragma solidity >=0.7.0 < 0.9.0;
+pragma solidity >=0.7.0 <0.9.0;
 
-contract Voiting{
+contract Voiting {
+    address owener;
+    uint256 CondidateCount;
 
-    struct Condidate{
+    struct Condidate {
+        uint256 id;
         string name;
         uint128 voteCount;
     }
 
-    
+    mapping(uint256 => Condidate) candidates;
+    mapping(address => bool) voted;
 
+    constructor() public {
+        owener = msg.sender;
+    }
+
+    function addCandidate() public {}
+
+    function vote(uint256 id) public {}
+
+    function Result() public {}
 }
-
